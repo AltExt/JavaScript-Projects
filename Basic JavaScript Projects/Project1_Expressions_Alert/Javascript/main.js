8fe12be0da0705269118bb6f49635dd0518c92b7
AltExt <52500808+AltExt@users.noreply.github.com>
2023-12-07 17:44:22 +0000

    First JS commit

---
+++ Basic JavaScript Projects/Project1_Expressions_Alert/Javascript/main.js
@@ -0,0 +1,8 @@
+function ButonPress() {
+	var s1 = "This is the beginning of the string ";
+	var s2 = "and this is the end of the string";
+	alert(s1); // sends alert with s1
+	alert(s2); // sends alert with s2
+	var fullString = s1 + s2; // concatonate strings
+	document.write(fullString); // print to document
+}

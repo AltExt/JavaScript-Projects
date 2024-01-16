563e5784c1c24c09d07619c9605731cf6bab3330
AltExt <52500808+AltExt@users.noreply.github.com>
2024-01-16 12:14:29 +0000

    Finished ttt project as I wanted it, starting TA version

--- Basic JavaScript Projects/TicTacToe Game/JS/main.js
+++ Basic JavaScript Projects/TicTacToe Game/JS/main.js
@@ -1,14 +1,60 @@
 let PlayerSelection = "x";
 let ComputerSelection = "o";
+
 let Mode = 0;
-let Difficulty = 0;
+let Difficulty = 2;
+let PlayerLastMove = 0;
+let PlayerCanInteract = false;
+
 const X_SQUARE = "X-Square";
 const O_SQUARE = "O-Square";
 const cols = ["L", "M", "R"];
-const rows = ["Top", "Mid", "Bottom"];
+const rows = ["Top", "Mid", "Bot"];
+
+const PVP_MODE = 0;
+const PVE_MODE = 1;
+
+const LOW_DIFF = 0;
+const MED_DIFF = 1;
+const HIGH_DIFF = 2;
+
 
 function RandomWithRange(minimum = 0.0, maximum = 1.0) {
 	return (Math.random() * (maximum - minimum)) + minimum;
+}
+
+function GoToPlayerSelect() {
+	document.getElementById("playButton").style.visibility = "hidden";
+	
+	document.getElementById("twoPlayerModeButton").style.visibility = "visible";
+	document.getElementById("computerModeButton").style.visibility = "visible";
+}
+
+
+function SetMode(mode) {
+	Mode = mode;
+	
+	document.getElementById("twoPlayerModeButton").style.visibility = "hidden";
+	document.getElementById("computerModeButton").style.visibility = "hidden";
+	
+	if (mode == PVP_MODE) {
+		GeneratePlayField();
+		return;
+	}
+	
+	document.getElementById("lowDiffButton").style.visibility = "visible";
+	document.getElementById("medDiffButton").style.visibility = "visible";
+	document.getElementById("highDiffButton").style.visibility = "visible";
+}
+
+function SetDiff(diff) {
+	Difficulty = diff;
+	
+	document.getElementById("lowDiffButton").style.visibility = "hidden";
+	document.getElementById("medDiffButton").style.visibility = "hidden";
+	document.getElementById("highDiffButton").style.visibility = "hidden";
+	
+	GeneratePlayField();
 }
 
 function GeneratePlayField() {
@@ -56,7 +102,7 @@ function GeneratePlayField() {
 		}
 		targetDiv.appendChild(col);
 	}
-
+	PlayerCanInteract = true;
 	AddOnClickEvents();
 }
 
@@ -70,7 +116,6 @@ function ClearPlayField() {
 			break;
 		}
 	}
-	GeneratePlayField();
 }
 
 function AddOnClickEvents() {
@@ -117,28 +162,76 @@ function OnMouseLeaveFunc(element) {
 }
 
 function OnClickFunc(element) {
-	let className = element.className;
-	if (className.includes(X_SQUARE) || className.includes(O_SQUARE)) {
-		return;
-	}
-	let s = " ";
-	if (PlayerSelection == "x") {
-		s += X_SQUARE;
-	}
-	else {
-		s += O_SQUARE;
-	}
+	if (PlayerCanInteract) {
+		let className = element.className;
+		if (className.includes(X_SQUARE) || className.includes(O_SQUARE)) {
+			return;
+		}
+		let s = " ";
+		if (PlayerSelection == "x") {
+			s += X_SQUARE;
+		}
+		else {
+			s += O_SQUARE;
+		}
 
-	element.className += s;
-	let id = element.id.substring(2);
-	id = "img" + id;
-	let img = document.getElementById(id);
-	img.style.opacity = 1.0;
+		element.className += s;
+		let id = element.id.substring(2);
 
-	CheckForWin();
+		// here we can get the player's actual move:
+		let row = id.substring(1, 4);
+		let col = id.substring(5);
+		let i = 0;
+		let j = 0;
+		for (i = 0; i < rows.length; i++) {
+			if (row == rows[i]) {
+				break;
+			}
+		}
+		for (j = 0; j < cols.length; j++) {
+			if (col == cols[j]) {
+				break;
+			}
+		}
+
+		PlayerLastMove = i + (3 * j);
+
+		id = "img" + id;
+		let img = document.getElementById(id);
+		img.style.opacity = 1.0;
+
+		RunOtherTurn();
+}
 }
 
-function CheckForWin() {
+function RunOtherTurn() {
+	// check if the player's move won, if not then take a computer turn and check that before passing back to the player, or reset the player's selection for 2 player mode
+
+	if ( CheckForWin() ) {
+		// game over, return
+		AllowPlayerToPlayAgain();
+	}
+	else {
+		// here either a computer move or the second player gains control
+		if (Mode == PVE_MODE) {
+			ComputersTurn();
+		}
+		else {
+			// mode is PVP_MODE
+			if (PlayerSelection == "x") {
+				PlayerSelection = "o";
+			}
+			else {
+				PlayerSelection = "x";
+			}
+		}
+		if (CheckForWin()) {
+			AllowPlayerToPlayAgain();
+		}
+	}
+}
+
+function GetBoardState() {
 	// get the board and store each cell as 1 0 or -1
 
 	let board = [];
@@ -155,6 +248,13 @@ function CheckForWin() {
 			board.push(0);
 		}
 	}
+
+	return board;
+}
+
+function CheckForWin() {
+
+	let board = GetBoardState();
 
 	let hasSomeOneWon = false;
 	let winner = 0;
@@ -230,27 +330,13 @@ function CheckForWin() {
 		else {
 			alert("Draw!");
 		}
-		return;
+		return true;
 	}
-	else {
-		console.log("in here");
-		if ( Mode == 0 ) {
-			ComptersTurn(board);
-		}
-		else {
-			if (PlayerSelection == "x") {
-				console.log("setting player selection to o");
-				PlayerSelection = "o";
-			}
-			else {
-				PlayerSelection = "x";
-			}
-		}
-	}
+	else return false;
 }
 
 
-function ComptersTurn(board) {
+function ComputersTurn() {
 	/*  board is as follows: 
 		0	3	6
 		1	4	7
@@ -261,20 +347,25 @@ function ComptersTurn(board) {
 		hard should try to tie / win every game
 	*/
 
+	let board = GetBoardState();
+
 	let compNumber = (PlayerSelection == "x") ? -1: 1;
 	let move = 0;
+	console.log(board);
+	console.log("comp num: " + compNumber);
 	switch(Difficulty) {
-		case 0:
+		case LOW_DIFF:
 			move = CompTurnEasy(board, compNumber);
 			break;
-		case 1:
+		case MED_DIFF:
 			move = CompTurnMedium(board, compNumber);
 			break;
-		case 2:
+		case HIGH_DIFF:
 			move = CompTurnHard(board, compNumber);
 			break;
 		default:
 			move = CompTurnEasy(board, compNumber);
+			break;
 	}
 	ComputerPlayMove(move);
 }
@@ -296,11 +387,68 @@ function CompTurnEasy(board, compNumber) {
 }
 
 function CompTurnMedium(board, compNumber) {
+	let winningMoves = GetWinningMoves(board, compNumber);
+	if (winningMoves.length != 0) {
+		console.log("Winning move found 1!");
+		return winningMoves[0];
+	}
 
+	let threats = GetWinningMoves(board, compNumber * -1);
+	if (threats.length != 0) {
+		return threats[0];
+	}
+	
+	while (true) {
+		let r = Math.floor(RandomWithRange(0, 9));
+
+		if (board[r] == 0) {
+			return r;
+		}
+	}
 }
 
 function CompTurnHard(board, compNumber) {
+	// if this is the first turn, all we need to do is make sure we make a move in one of the diagonals
+	let winningMoves = GetWinningMoves(board, compNumber);
+	if (winningMoves.length != 0) {
+		console.log("Winning move found 1!");
+		return winningMoves[0];
+	}
 
+	let threats = GetWinningMoves(board, compNumber * -1);
+	if (threats.length != 0) {
+		return threats[0];
+	}
+	
+	let corners = [0, 2, 6, 8]
+	let middles = [1, 3, 5, 7];
+	let centre = 4;
+	// at this point, its most likely best to secure the centre, then a corner, then a middle 
+
+	if (board[centre] == 0) {
+		return centre;
+	}
+	else {
+		for (let i = 0; i < corners.length; i++) {
+			if (board[corners[i]] == 0) {
+				return corners[i];
+			}
+		}
+
+		for (let i = 0; i < middles.length; i++) {
+			if (board[middles[i]] == 0) {
+				return middles[i]
+			}
+		}
+	}
+
+	while (true) {
+		let r = Math.floor(RandomWithRange(0, 9));
+
+		if (board[r] == 0) {
+			return r;
+		}
+	}
 }
 
 function ComputerPlayMove(move) {
@@ -329,42 +477,175 @@ function ComputerPlayMove(move) {
 }
 
 function GetWinningMoves(board, player) {
+	/*  board is as follows: 
+		0	3	6
+		1	4	7
+		2	5	8
+	*/
 	let output = [];
+	let bool = false;
 	const TARGET = 2 * player;
+
 	// vertical threats
+	if (bool) {
+		let v1Threat1 = board[0] + board[1];
+		let v1Threat2 = board[1] + board[2];
+		let v1Threat3 = board[0] + board[2];
+		let v1Pos1 = board[2];
+		let v1Pos2 = board[0];
+		let v1Pos3 = board[1];
+
+		let v2Threat1 = board[3] + board[4];
+		let v2Threat2 = board[4] + board[5];
+		let v2Threat3 = board[3] + board[5];
+		let v2Pos1 = board[4];
+		let v2Pos2 = board[3];
+		let v2Pos3 = board[5];
+
+		let v3Threat1 = board[6] + board[7];
+		let v3Threat2 = board[7] + board[8];
+		let v3Threat3 = board[6] + board[8];
+		let v3Pos1 = board[7];
+		let v3Pos2 = board[6];
+		let v3Pos3 = board[8];
+
+		console.log("v1:");
+		console.log( v1Threat1 + " - " + v1Pos1 );
+		console.log( v1Threat2 + " - " + v1Pos2 );
+		console.log( v1Threat3 + " - " + v1Pos3 );
+		
+		console.log("v2:");
+		console.log( v2Threat1 + " - " + v2Pos1 );
+		console.log( v2Threat2 + " - " + v2Pos2 );
+		console.log( v2Threat3 + " - " + v2Pos3 );
+		
+		console.log("v3:");
+		console.log( v3Threat1 + " - " + v3Pos1 );
+		console.log( v3Threat2 + " - " + v3Pos2 );
+		console.log( v3Threat3 + " - " + v3Pos3 );
+	}
+
 	for (let i = 0; i < board.length; i += 3) {
-		/* */if (board[i] + board[i+1] == TARGET && board[i+2] == 0) {
+		if (board[i] + board[i+1] == TARGET && board[i+2] == 0) {
 			output.push(i+2);
 		}
-		else if (board[i+1] + board[i+2] == TARGET && board[i] == 0) {
+		if (board[i+1] + board[i+2] == TARGET && board[i] == 0) {
 			output.push(i);
+		}
+		if (board[i] + board[i+2] == TARGET && board[i+1] == 0) {
+			output.push(i+1);
 		}
 	}
 
 	// horizontal threats
+	if (bool) {
+		let h1Threat1 = board[0] + board[3];
+		let h1Threat2 = board[3] + board[6];
+		let h1Threat3 = board[0] + board[6];
+		let h1Pos1 = board[6];
+		let h1Pos2 = board[0];
+		let h1Pos3 = board[3];
+		
+		let h2Threat1 = board[1] + board[4];
+		let h2Threat2 = board[4] + board[7];
+		let h2Threat3 = board[1] + board[7];
+		let h2Pos1 = board[7];
+		let h2Pos2 = board[1];
+		let h2Pos3 = board[4];
+		
+		let h3Threat1 = board[2] + board[5];
+		let h3Threat2 = board[5] + board[8];
+		let h3Threat3 = board[2] + board[8];
+		let h3Pos1 = board[8];
+		let h3Pos2 = board[2];
+		let h3Pos3 = board[5];
+
+		console.log("h1:");
+		console.log( h1Threat1 + " - " + h1Pos1 );
+		console.log( h1Threat2 + " - " + h1Pos2 );
+		console.log( h1Threat3 + " - " + h1Pos3 );
+		
+		console.log("h2:");
+		console.log( h2Threat1 + " - " + h2Pos1 );
+		console.log( h2Threat2 + " - " + h2Pos2 );
+		console.log( h2Threat3 + " - " + h2Pos3 );
+		
+		console.log("h3:");
+		console.log( h3Threat1 + " - " + h3Pos1 );
+		console.log( h3Threat2 + " - " + h3Pos2 );
+		console.log( h3Threat3 + " - " + h3Pos3 );
+	}
+
 	for (let i = 0; i < 3; i++) {
-		/* */if (board[i] + board[i+3] == TARGET && board[i+6] == 0) {
+		if (board[i] + board[i+3] == TARGET && board[i+6] == 0) {
 			output.push(i+6);
 		}
-		else if (board[i+3] + board[i+6] == TARGET && board[i] == 0) {
+		if (board[i+3] + board[i+6] == TARGET && board[i] == 0) {
 			output.push(i);
+		}
+		if (board[i] + board[i+6] == TARGET && board[i+3] == 0) {
+			output.push(i+3);
 		}
 	}
 
 	// diagonal threats
-	/* */if (board[0] + board[4] == TARGET && board[8] == 0) {	
-		output.push(8);
-	}
-	else if (board[4] + board[8] == TARGET && board[0] == 0) {	
-		output.push(0);
+	if (bool) {
+		let d1Threat1 = board[0] + board[4];
+		let d1Threat2 = board[0] + board[8];
+		let d1Threat3 = board[4] + board[8];
+		let d1Pos1 = board[8];
+		let d1Pos2 = board[4];
+		let d1Pos3 = board[0];
+
+		let d2Threat1 = board[2] + board[4];
+		let d2Threat2 = board[2] + board[6];
+		let d2Threat3 = board[4] + board[6];
+		let d2Pos1 = board[6];
+		let d2Pos2 = board[4];
+		let d2Pos3 = board[2];
+
+		console.log("d1:");
+		console.log( d1Threat1 + " - " + d1Pos1 );
+		console.log( d1Threat2 + " - " + d1Pos2 );
+		console.log( d1Threat3 + " - " + d1Pos3 );
+		
+		console.log("d2:");
+		console.log( d2Threat1 + " - " + d2Pos1 );
+		console.log( d2Threat2 + " - " + d2Pos2 );
+		console.log( d2Threat3 + " - " + d2Pos3 );
 	}
 
-	/* */if (board[6] + board[4] == TARGET && board[2] == 0) {
+	if (board[0] + board[4] == TARGET && board[8] == 0) {	
+		output.push(8);
+	}
+	if (board[4] + board[8] == TARGET && board[0] == 0) {	
+		output.push(0);
+	}
+	if (board[0] + board[8] == TARGET && board[4] == 0) {
+		output.push(4);
+	}
+
+	if (board[6] + board[4] == TARGET && board[2] == 0) {
 		output.push(2);
 	}
-	else if (board[2] + board[4] == TARGET && board[6] == 0) {
+	if (board[2] + board[4] == TARGET && board[6] == 0) {
 		output.push(6);
+	}
+	if (board[2] + board[6] == TARGET && board[4] == 0) {
+		output.push(4);
 	}
 
 	return output;
+}
+
+function AllowPlayerToPlayAgain() {
+	PlayerCanInteract = false;
+	document.getElementById("playAgainButton").style.visibility = "visible";
+}
+
+function PlayAgian() {
+	ClearPlayField();
+	
+	document.getElementById("playAgainButton").style.visibility = "hidden";
+	document.getElementById("playButton").style.visibility = "visible";
 }
